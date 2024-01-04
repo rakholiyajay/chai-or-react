674fb6bdeecd049d31527f3e1096395072701d30
Rakholiya jay <119782456+rakholiyajay@users.noreply.github.com>
2024-01-04 12:20:57 +0530

    React router crash course

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  const data = useLoaderData();
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/rakholiyajay")
+  //       .then((responce) => responce.json())
+  //       .then((data) => {
+  //         console.table(data);
+  //         setData(data);
+  //       });
+  //   }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github Followers: {data.followers}
+      <img src={data.avatar_url} alt="Git Picture" width={300} />
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const responce = await fetch("https://api.github.com/users/rakholiyajay");
+  return responce.json();
+};
