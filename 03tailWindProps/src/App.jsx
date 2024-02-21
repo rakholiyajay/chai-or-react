e661138853d7f70b7e1f6c9f9312ec891d043e1a
rakholiyajay <rakholiyajay147@gmail.com>
2024-02-21 20:17:55 +0530

    03 tailwind with props

---
+++ 03tailWindProps/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test
+      </h1>
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+export default App;
