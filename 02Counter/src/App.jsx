36fe6df5fbd1a0108fee19fc96abc17e94173a63
rakholiyajay <rakholiyajay147@gmail.com>
2024-02-21 18:50:42 +0530

    02 Counter project > usestate hook learning

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  //let counter = 5;
+
+  const addValue = () => {
+    // counter = counter + 1;
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+      <p>Footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
