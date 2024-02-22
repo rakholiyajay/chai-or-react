b9defb717980dc8268302733cf4bbaf80598dfe1
rakholiyajay <rakholiyajay147@gmail.com>
2024-02-22 12:22:15 +0530

    03Tailwind with props  > Tailwind css  >props  >default value  >{} variable write in curly braces

--- 03tailWindProps/src/App.jsx
+++ 03tailWindProps/src/App.jsx
@@ -5,13 +5,28 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  //passing the value in the props
+  //pass the object
+  let myObj = {
+    userName: "jay",
+    age: 21,
+  };
+  //pass the array
+  let newArr = [1, 2, 3];
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
         Tailwind Test
       </h1>
-      <Card />
-      <Card />
+      <Card
+        userName="chaiaurcode"
+        btntext="clickMe"
+        img="https://images.unsplash.com/photo-1696176559416-ef8d7115e3ba?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Card
+        userName="Jay"
+        img="https://images.unsplash.com/photo-1696176559269-c944fb2ec40f?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
     </>
   );
 }
--- 03tailWindProps/src/components/Card.jsx
+++ 03tailWindProps/src/components/Card.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 
-function Card() {
+function Card({ userName, btntext = "visitMe", img }) {
+  console.log(userName, btntext, img);
   return (
-    <div class="relative h-[400px] w-[300px] rounded-md">
+    <div className="relative h-[400px] w-[300px] rounded-md mt-4">
       <img
-        src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+        src={img}
         alt="AirMax Pro"
-        class="z-0 h-full w-full rounded-md object-cover"
+        className="z-0 h-full w-full rounded-md object-cover"
       />
-      <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-      <div class="absolute bottom-4 left-4 text-left">
-        <h1 class="text-lg font-semibold text-white">Delba</h1>
-        <p class="mt-2 text-sm text-gray-300">
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <div className="absolute bottom-4 left-4 text-left">
+        <h1 className="text-lg font-semibold text-white">{userName}</h1>
+        <p className="mt-2 text-sm text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
           debitis?
         </p>
-        <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-black">
-          View Profile →
+        <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-black">
+          {btntext} →
         </button>
       </div>
     </div>
