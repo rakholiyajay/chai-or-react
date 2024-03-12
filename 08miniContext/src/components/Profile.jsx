a2a5bcc4dd562b7be84d16e5c11deaefb1bdbd06
rakholiyajay <rakholiyajay147@gmail.com>
2024-03-12 12:22:35 +0530

    08 UseContext

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <div>please Login</div>;
+
+  return <div>Welcome {user.username}</div>;
+}
+
+export default Profile;
